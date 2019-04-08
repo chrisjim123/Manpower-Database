3c9b323ccb23a1e062e2f18c23037cc57fd5493c
chrisjim1231 <sabosidochrisjim@gmail.com>
2019-04-09 01:24:42 +0800

    Updated april 9, 2019

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->varchar('usertype');
+            $table->text('usertype');
         });
     }
 
